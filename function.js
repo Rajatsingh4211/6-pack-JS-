4af069a2b2812d2ca6538dbefa8c9cf829fc0870
Rajatsingh4211 <141514027+Rajatsingh4211@users.noreply.github.com>
2023-12-04 16:55:15 +0530

    Add files via upload
    Functions in JavaScript by 6 pack programmer.

---
+++ function.js
@@ -0,0 +1,15 @@
+// Trying to push on Github
+
+
+
+
+
+let result = (23*10 + 23*10 + 23*10)/10 ;
+
+console.log(result);
+
+let result2 = (45*10 + 45*10 + 45*10)/10 ;
+console.log(result2);
+
+// Functions in JS 
+
---
+++ test.js
@@ -0,0 +1,24 @@
+// let arr = []
+
+// for(let i = 0; i<100; ++i){
+//     arr [i] = "Rajat"
+// }
+// console.log(arr); 
+const myObj = {
+    name: "Rajat", 
+    surname: "Singh",
+    age: 27, 
+    male: true
+}
+myObj.surname = "Kumar"
+myObj.newData = "Beta"
+
+console.log(myObj);
+
+const myObj2 = {
+    0: "Rajat", 
+    1: "Singh",
+    2: 27, 
+    3: true
+}
+console.log(myObj);
