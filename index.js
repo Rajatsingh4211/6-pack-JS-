68d0ad78eb424ea76afb5c2a4ebfc2c321d8f0a5
Rajatsingh4211 <141514027+Rajatsingh4211@users.noreply.github.com>
2023-12-04 16:53:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+
+// let iceCream  = Number (prompt ("Type a number"));
+
+// Arithmetic Operators
+// +, -, *, %, =
+
+// Comparision Operators. 
+// ==, >, <, >=, <=, !=
+
+// if (iceCream===10){
+//     console.log('IceCream is Blue Lagoon');
+// }
+
+// else if(iceCream===30){
+//     console.log('IceCream is cone')
+// }
+
+// else{
+//     console.log('This is a vanilla iceCream');
+// }
+// if(iceCream = 10){
+// console.log("IceCream is Blue Lagoon")
+
+// }
+
+// console.log("IceCream is Cone");
+// switch (iceCream) {
+//     case 10:
+//         console.log('IceCream is Blue Lagoon');
+//         break;
+
+//         case 30:
+//         console.log('IceCream is cone'); 
+//         break;
+
+//     default:
+//             console.log('This is a vanilla iceCream');
+//         break;
+// }
+// Loops in JavaScript.
+
+// for(let i=0; i<10; i = i+1){
+//     console.log(Math.random());
+//     console.log("Hi");
+// }
+let s = "My name is Rajat"
+let heading = document.getElementById("head"); 
+console.log(heading);
+
+heading.innerText = s;
+heading.style.backgroundColor = "red";
+heading.style.color =" White"
---
+++ try.js
@@ -0,0 +1,12 @@
+// var subOne = Number(prompt("Type a subone marks"));
+// var subTwo = Number(prompt("Type a subTwo marks"));
+
+// // Avoid using 'let Number' in this context, as it's unnecessary
+// // Let's directly calculate and log the percentage
+// console.log(((subOne + subTwo) / 200) * 100);
+
+// const gameName = new String("Rajat")
+// console.log(gameName.toUpperCase); 
+
+let x =  "my name is Abhi"
+console.log(x. toUpperCase);
